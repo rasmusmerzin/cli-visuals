2b5cbeabe1caac3df7eb4d483c741d307dd237d2
keep <keep@keep.lan>
2019-02-19 09:47:29 +0200

    lines update

--- lines.py
+++ lines.py
@@ -5,16 +5,17 @@ import os, random, time
 
 
 def get_screen_size():
-    return tuple(int(x) for x in os.popen("stty size", "r").read().split())
+    return [int(x) for x in os.popen("stty size", "r").read().split()]
 
 def draw(txt, pos=0):
-    print((pos and ("\033[" +str(pos[0]) +";" +str(pos[1]) +"H") or "") +txt, end="")
+    print((pos and ("\033[" +str(pos[0]) +";" +str(pos[1]) +"H") or "") +txt)
 
 def clear():
     os.system("clear")
 
 def draw_line():
     screen_size = get_screen_size()
+    screen_size[0] -= 1
 
     c = "\033[" +str(random.randint(31, 37)) +"m"
     h = random.randint(1, screen_size[0] +screen_size[1] -2)
@@ -24,15 +25,14 @@ def draw_line():
     if h > screen_size[0]:
         for i in range(1, screen_size[0] +1):
             draw(c +"|" +chr(random.randint(33, 126)) +"|\033[0m", (dir == 1 and i or (screen_size[0] -i +1), h -screen_size[0]))
-            #if i %60: time.sleep(.03)
+            if i %8 == 0: time.sleep(.03)
     else:
         for i in range(1, screen_size[1] +1):
             draw(c +"-\033[0m", (h, dir == 1 and i or (screen_size[1] -i +1)))
-            #if i %120: time.sleep(.03)
+            if i %40 == 0: time.sleep(.03)
 
 
 clear()
 
 while 1:
     draw_line()
-    time.sleep(.1)
