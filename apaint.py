f7c204b7a6f367927d6a4ae8bf5f23d417c60d5d
rasmusmerzin <rasmusmerzin@gmail.com>
2019-03-18 15:04:34 +0200

    apaint: run <cmd>

--- apaint.py
+++ apaint.py
@@ -9,7 +9,7 @@ timemark = "{0}-{1}-{2}_{3}-{4}-{5}".format(lt.tm_year, lt.tm_mon, lt.tm_mday, l
 
 
 def main(scr):
-    global y, x, autopush, content, colortable, bar_title, running, timemark, status
+    global y, x, autopush, content, colortable, bar_title, running, timemark, status, cmd_log
 
     curses.start_color()
     curses.use_default_colors()
@@ -24,6 +24,9 @@ def main(scr):
 
     content = {}
     colortable = {}
+
+    cmd_log = []
+    cmdi = -1
     
 
     for i in range(7): curses.init_pair(i, i, -1)
@@ -109,22 +112,37 @@ def main(scr):
         if cmd[0] == "autopush":
             if len(cmd) > 1: autopush = cmd[1]
             else: bar_title[0] = cmd[0] +": direction (D|U|L|R|-) required"
+
         elif cmd[0] == "status":
             status = 1 -status
             if not status:
                 time.sleep(.1)
                 bar_title[0] = ""
+
         elif cmd[0] == "q!": running = 0
+
+        elif cmd[0] == "r" or cmd[0] == "run":
+            try:
+                lns = os.popen(" ".join(cmd[1:])).read().split("\n")
+                for i in range(len(lns)):
+                    scr.addstr(shadow[0] +i, shadow[1], lns[i])
+            except: bar_title[0] = "command error"
+
         elif cmd[0] == "clear!":
             content, colortable = {}, {}
             scr.addstr(0, 0, ("\n" +" " *(scr.getmaxyx()[1] -1)) *(scr.getmaxyx()[0] -1))
+            os.system("clear")
+            print("\033[" +str(scr.getmaxyx()[0] -1) +"H")
+
         elif cmd[0] == "w" or cmd[0] == "write" or cmd[0] == "wq":
             if len(cmd) > 1: write_to_file(cmd[1])
             else: write_to_file()
             if cmd[0] == "wq": running = 0
+
         elif cmd[0] == "l" or cmd[0] == "load":
             if len(cmd) > 1: load_from_file(cmd[1])
             else: bar_title[0] = cmd[0] +": file name required"
+
         elif cmd[0] == "q" or cmd[0] == "exit" or cmd[0] == "quit": bar_title[0] = "use 'q!' or 'wq'"
         else: bar_title[0] = "no command '{0}'".format(cmd[0])
 
@@ -150,10 +168,13 @@ def main(scr):
             elif c == 262: x = 0
             elif c == 360: x = scr.getmaxyx()[1]
 
+            elif c == 9: scheme = (scheme +1) %7
+
             shadow = [y, x]
 
         if mode != 0:
             if c == 27:
+                if mode == -1: cmd_log = cmd_log[:-1]
                 mode = 0
                 bar_title = [""]
                 y, x = shadow[0], shadow[1]
@@ -167,6 +188,8 @@ def main(scr):
                 bar_title = ["-- SELECT --", 1]
                 corner = [y, x]
             elif c == 58:
+                cmdi = -1
+                cmd_log += [":"]
                 mode = -1
                 bar_title = [":"]
                 y, x = scr.getmaxyx()[0] -1, 1
@@ -185,16 +208,34 @@ def main(scr):
                 if c != 330:
                     y += autopush.count("d") -autopush.count("u")
                     x += autopush.count("r") -autopush.count("l")
-            elif c == 9: scheme = (scheme +1) %7
         elif mode == -1:
             if c > 31 and c < 127:
-                bar_title[0] += chr(c)
+                bar_title[0] = bar_title[0][:x] +chr(c) +bar_title[0][x:]
                 x += 1
+
+            elif c == 261: x += x < len(bar_title[0]) and 1 or 0
+            elif c == 260: x -= x > 1 and 1 or 0
+            elif c == 262: x = 1
+            elif c == 360: x = len(bar_title[0])
+
             elif c == 263:
-                if len(bar_title[0]) > 1:
-                    bar_title[0] = bar_title[0][:-1]
+                if x > 1:
+                    bar_title[0] = bar_title[0][:x-1] +bar_title[0][x:]
                     x -= 1
+            elif c == 330: bar_title[0] = bar_title[0][:x] +bar_title[0][x+1:]
+
+            elif c == 258:
+                cmdi = min(-1, cmdi +1)
+                bar_title[0] = cmd_log[cmdi]
+                x = len(cmd_log[cmdi])
+            elif c == 259:
+                cmdi = max(-len(cmd_log), cmdi -1)
+                bar_title[0] = cmd_log[cmdi]
+                x = len(cmd_log[cmdi])
+
             elif c == 10: #ADD: or c == <numpad_enter>
+                if len(cmd_log) > 2 and cmd_log[-2] == bar_title[0] or cmd_log[cmdi] == bar_title[0]: del cmd_log[-1]
+                else: cmd_log[-1] = bar_title[0]
                 cmd = bar_title[0][1:]
                 bar_title[0] = ">" +cmd
                 execute(*cmd.lower().split())
