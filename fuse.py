707dfea9fa63d870b1f509fb60ef9f009392271e
rasmusmerzin <rasmusmerzin@gmail.com>
2019-04-06 18:41:50 +0100

    autopush

--- fuse.py
+++ fuse.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 
-import os, sys, random, time, signal
+import os, sys, random, time, signal, math
 
 
 print("\033[?25l")
@@ -24,10 +24,10 @@ def init():
 
 chars = [
     [""],
-    [".", "`", "'"],
-    ["-", '"'],
-    ["*", ";", ":", "~"],
-    ["$", "&", "="],
+    [".", "'"],
+    [":", ";"],
+    ["*", "="],
+    ["$", "&"],
     ["#", "@", "%"]
 ]
 
@@ -38,19 +38,30 @@ def char(p):
             return chars[i][random.randint(0, len(chars[i]) -1)]
     return ""
 
+def ang2vec(a, r): return [int(math.cos(a) *r), int(math.sin(a) *r *1.5)]   #y, x
+
 def dis(p0, p1): return ((abs(p0[0] -p1[0]) *2) **2 +abs(p0[1] -p1[1]) **2) **.5
 
-def dot(y, x, r):
+def dot(y, x, r, c=37):
     for m in range(r *2 +1):
         for n in range(r *4 +1):
             p = [y -r +m, x -r *2 +n]
-            print("\033[" +str(p[0]) +";" +str(p[1]) +"H" +char(dis([y, x], p) /r))
+            ch = char(dis([y, x], p) /r)
+            if ch: print("\033[" +str(c) +"m\033[" +str(p[0]) +";" +str(p[1]) +"H" +ch +"\033[0m")
 
 
 
 init()
 
+pos = [int(scr[0] /2), int(scr[1] /2)]
+ang = 90
+
 while 1:
-    time.sleep(.04)
-    r = random.randint(3, 5)
-    dot(random.randint(r +1, scr[0] -r -1), random.randint(r +1, scr[1] -r -3), r)
+    d = random.randint(-60, 60) *math.pi /360
+    c = random.randint(31, 36)
+    for _ in range(8):
+        ang += d
+        vec = ang2vec(ang, 3)
+        pos = [min(scr[0] -4, max(4, pos[0] +vec[0])), min(scr[1] -4, max(4, pos[1] +vec[1]))]
+        time.sleep(.04)
+        dot(pos[0], pos[1], 6, c)
