a20b39218502b70513166a394a3ee2f01f20b081
rasmusmerzin <rasmusmerzin@gmail.com>
2019-04-08 12:53:35 +0100

    fuse.py

--- fuse.py
+++ fuse.py
@@ -26,9 +26,9 @@ chars = [
     [""],
     [".", "'"],
     [":", ";"],
-    ["*", "="],
+    ["*", "#"],
     ["$", "&"],
-    ["#", "@", "%"]
+    ["#"]
 ]
 
 
@@ -40,28 +40,52 @@ def char(p):
 
 def ang2vec(a, r): return [int(math.cos(a) *r), int(math.sin(a) *r *1.5)]   #y, x
 
+def vec2ang(v): return math.atan(v[1] /v[0]) +(v[0] < 0 and 180 or 0)
+
 def dis(p0, p1): return ((abs(p0[0] -p1[0]) *2) **2 +abs(p0[1] -p1[1]) **2) **.5
+
+def tile2win(p): return [p[0] %(scr[0] -1) +1, p[1] %scr[1] +1]
+
+
+trail = []
+colors = [1, 5, 4, 6, 2, 3]
 
 def dot(y, x, r, c=37):
     for m in range(r *2 +1):
         for n in range(r *4 +1):
-            p = [y -r +m, x -r *2 +n]
+            p = tile2win([y -r +m, x -r *2 +n])
             ch = char(dis([y, x], p) /r)
-            if ch: print("\033[" +str(c) +"m\033[" +str(p[0]) +";" +str(p[1]) +"H" +ch +"\033[0m")
+            ch = ch == "#" and chr(random.randint(33, 126)) or ch
+            if ch:
+                print("\033[" +str(30 +colors[c]) +"m\033[" +str(p[0]) +";" +str(p[1]) +"H" +ch +"\033[0m")
+                trail.insert(0, p)
+
+def cleanup():
+    length = 8000
+    for i in range(len(trail) -1, length, -1):
+        if random.randint(0, 1) == 0:
+            if trail[:length].count(trail[i]) == 0:
+                print("\033[" +str(trail[i][0]) +";" +str(trail[i][1]) +"H ")
+            trail.pop(i)
 
 
 
 init()
 
 pos = [int(scr[0] /2), int(scr[1] /2)]
-ang = 90
+ang = 0
+c = random.randint(0, 5)
 
 while 1:
-    d = random.randint(-60, 60) *math.pi /360
-    c = random.randint(31, 36)
-    for _ in range(8):
-        ang += d
-        vec = ang2vec(ang, 3)
-        pos = [min(scr[0] -4, max(4, pos[0] +vec[0])), min(scr[1] -4, max(4, pos[1] +vec[1]))]
-        time.sleep(.04)
-        dot(pos[0], pos[1], 6, c)
+    c = (c +1) %6
+    for _ in range(4):
+        d = random.randint(-60, 60) *math.pi /360
+        for _ in range(8):
+            ang += d
+            vec = ang2vec(ang, 3)
+            pos = tile2win([pos[0] +vec[0], pos[1] +vec[1]])
+            time.sleep(.02)
+
+            dot(pos[0], pos[1], 5, c)
+
+            cleanup()
