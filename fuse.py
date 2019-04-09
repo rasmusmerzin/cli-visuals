d69622db487179508477521155b0326fa283d67c
rasmusmerzin <rasmusmerzin@gmail.com>
2019-04-09 18:02:57 +0100

    options

--- fuse.py
+++ fuse.py
@@ -50,7 +50,34 @@ def tile2win(p): return [p[0] %(scr[0] -1) +1, p[1] %scr[1] +1]
 trail = []
 colors = [1, 5, 4, 6, 2, 3]
 
+
+count = 1
+erase = 1
+tailsize = 1000
+
+for i in sys.argv[1:]:
+    if i[:3] == "-c=":
+        try: count = int(i[3:])
+        except: pass
+    elif i[:8] == "--count=":
+        try: count = int(i[8:])
+        except: pass
+    elif i[:3] == "-e=":
+        try: erase = int(i[3:])
+        except: pass
+    elif i[:8] == "--erase=":
+        try: erase = int(i[8:])
+        except: pass
+    elif i[:3] == "-t=":
+        try: tailsize = int(i[3:])
+        except: pass
+    elif i[:8] == "--tailsize=":
+        try: tailsize = int(i[11:])
+        except: pass
+
+
 def dot(y, x, r, c=37):
+    global erase, trail
     for m in range(r *2 +1):
         for n in range(r *4 +1):
             p = tile2win([y -r +m, x -r *2 +n])
@@ -58,43 +85,42 @@ def dot(y, x, r, c=37):
             ch = ch == "#" and chr(random.randint(33, 126)) or ch
             if ch:
                 print("\033[" +str(30 +colors[c]) +"m\033[" +str(p[0]) +";" +str(p[1]) +"H" +ch +"\033[0m")
-                trail.insert(0, p)
+                if erase: trail.insert(0, p)
 
 def cleanup():
-    length = 8000
+    global tailsize, trail
     spark = "%%%/!.^'"
-    for i in range(len(trail) -1, length, -1):
-        if i > length +100:
+    for i in range(len(trail) -1, tailsize, -1):
+        if i > tailsize +100:
             if random.randint(0, 1) == 0:
-                if trail[:length].count(trail[i]) == 0:
+                if trail[:tailsize].count(trail[i]) == 0:
                     print("\033[" +str(trail[i][0]) +";" +str(trail[i][1]) +"H ")
                 trail.pop(i)
         else:
             if random.randint(0, 10) == 0:
-                if trail[:length].count(trail[i]) == 0:
+                if trail[:tailsize].count(trail[i]) == 0:
                     print("\033[" +str(trail[i][0]) +";" +str(trail[i][1]) +"H" +spark[random.randint(0, len(spark) -1)])
-
-
 
 
 init()
 
-pos = [int(scr[0] /2), int(scr[1] /2)]
-ang = 0
-c = random.randint(0, 5)
+pos = [[int(scr[0] /2), int(scr[1] /2)]] *count
+ang = [0] *count
+c = []
+for _ in range(count): c += [random.randint(0, 5)]
 
-cu = len(sys.argv) > 1 and 1 or 0
+
 
 while 1:
-    c = (c +1) %6
+    for i in c: i = (i +1) %6
     for _ in range(4):
-        d = random.randint(-60, 60) *math.pi /360
+        d = []
+        for i in range(count): d += [random.randint(-60, 60) *math.pi /360]
         for _ in range(8):
-            ang += d
-            vec = ang2vec(ang, 3)
-            pos = tile2win([pos[0] +vec[0], pos[1] +vec[1]])
-            time.sleep(.02)
-
-            dot(pos[0], pos[1], 5, c)
-
-            if cu: cleanup()
+            for i in range(count):
+                ang[i] += d[i]
+                vec = ang2vec(ang[i], 3)
+                pos[i] = tile2win([pos[i][0] +vec[0], pos[i][1] +vec[1]])
+                time.sleep(.02)
+                dot(pos[i][0], pos[i][1], 5, c[i])
+            if erase: cleanup()
--- lines.py
+++ lines.py
@@ -22,11 +22,31 @@ def draw(txt, pos=0):
 def clear():
     os.system("clear")
 
+
+
+erase = 0
+tailsize = 500
 trail = []
-cu = len(sys.argv) > 1 and 1 or 0
+
+
+for i in sys.argv[1:]:
+    if i[:3] == "-e=":
+        try: erase = int(i[3:])
+        except: pass
+    elif i[:8] == "--erase=":
+        try: erase = int(i[8:])
+        except: pass
+    elif i[:3] == "-t=":
+        try: tailsize = int(i[3:])
+        except: pass
+    elif i[:8] == "--tailsize=":
+        try: tailsize = int(i[11:])
+        except: pass
 
 
 def draw_line():
+    global erase, trail
+
     c = "\033[" +str(random.randint(31, 37)) +"m"
     h = random.randint(1, screen_size[0] +screen_size[1] -2)
 
@@ -36,35 +56,35 @@ def draw_line():
         for i in range(1, screen_size[0] +1):
             p = (d == 1 and i or (screen_size[0] -i +1), h -screen_size[0])
             draw(c +"|" +chr(random.randint(33, 126)) +"|\033[0m", p)
-            trail.insert(0, p)
-            trail.insert(0, (p[0], p[1] +1))
-            trail.insert(0, (p[0], p[1] +2))
+            if erase:
+                trail.insert(0, p)
+                trail.insert(0, (p[0], p[1] +1))
+                trail.insert(0, (p[0], p[1] +2))
             if i %8 == 0:
                 time.sleep(.03)
-                if cu: cleanup()
+                if erase: cleanup()
     else:
         for i in range(1, screen_size[1] +1):
             p = (h, d == 1 and i or (screen_size[1] -i +1))
             draw(c +"%\033[0m", p)
-            trail.insert(0, p)
+            if erase: trail.insert(0, p)
             if i %40 == 0:
                 time.sleep(.03)
-                if cu: cleanup()
-
+                if erase: cleanup()
 
 
 def cleanup():
-    length = 6000
+    global tailsize, trail
     spark = "%%%/!.^'"
-    for i in range(len(trail) -1, length, -1):
-        if i > length +200:
+    for i in range(len(trail) -1, tailsize, -1):
+        if i > tailsize +200:
             if random.randint(0, 1) == 0:
-                if trail[:length].count(trail[i]) == 0:
+                if trail[:tailsize].count(trail[i]) == 0:
                     print("\033[" +str(trail[i][0]) +";" +str(trail[i][1]) +"H ")
                 trail.pop(i)
         else:
             if random.randint(0, 2) == 0:
-                if trail[:length].count(trail[i]) == 0:
+                if trail[:tailsize].count(trail[i]) == 0:
                     print("\033[" +str(trail[i][0]) +";" +str(trail[i][1]) +"H" +spark[random.randint(0, len(spark) -1)])
 
 
